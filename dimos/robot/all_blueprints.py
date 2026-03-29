2cf16f103c48303c38af668568d60f5f05358af7
Shubham Patil <shubhampatilsd@gmail.com>
2026-03-29 11:22:30 -0700

    fuck

--- dimos/robot/all_blueprints.py
+++ dimos/robot/all_blueprints.py
@@ -82,6 +82,7 @@ all_blueprints = {
     "unitree-go2": "dimos.robot.unitree.go2.blueprints.smart.unitree_go2:unitree_go2",
     "unitree-go2-agentic": "dimos.robot.unitree.go2.blueprints.agentic.unitree_go2_agentic:unitree_go2_agentic",
     "unitree-go2-agentic-duet": "dimos.robot.unitree.go2.blueprints.agentic.unitree_go2_agentic_duet:unitree_go2_agentic_duet",
+    "unitree-go2-agentic-duet-ota": "dimos.robot.unitree.go2.blueprints.agentic.unitree_go2_agentic_duet_ota:unitree_go2_agentic_duet_ota",
     "unitree-go2-agentic-huggingface": "dimos.robot.unitree.go2.blueprints.agentic.unitree_go2_agentic_huggingface:unitree_go2_agentic_huggingface",
     "unitree-go2-agentic-ollama": "dimos.robot.unitree.go2.blueprints.agentic.unitree_go2_agentic_ollama:unitree_go2_agentic_ollama",
     "unitree-go2-basic": "dimos.robot.unitree.go2.blueprints.basic.unitree_go2_basic:unitree_go2_basic",
